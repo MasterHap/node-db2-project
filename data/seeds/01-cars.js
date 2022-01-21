4c1e0c17bc5eb1dfb4102c68266e12f00cab6ed6
MasterHap <76802578+MasterHap@users.noreply.github.com>
2022-01-21 01:32:00 -0500

    Wip

--- data/seeds/01-cars.js
+++ data/seeds/01-cars.js
@@ -1,1 +1,31 @@
 // STRETCH
+const cars = [
+    {
+      vin: '11111111111111111',
+      make: 'toyota',
+      model: 'prius',
+      mileage: 215000,
+      title: 'clean',
+      transmission: 'manual',
+    },
+
+    {
+      vin: '11111111111111111',
+      make: 'toyota',
+      model: 'corolla',
+      mileage: 115000,
+      title: 'salvage',
+    },
+    {
+      vin: '11111111111111111',
+      make: 'toyota',
+      model: 'focus',
+      mileage: 15000,
+    },
+]
+exports.seed = function(knex) {
+    return knex('cars')
+    .truncate().then(() =>{
+     return knex('cars').insert(cars)
+    })
+}
