1d62f21ec06789f31491df308853d55b879d5c4c
MasterHap <76802578+MasterHap@users.noreply.github.com>
2022-01-21 02:41:16 -0500

    wip

--- api/cars/cars-router.js
+++ api/cars/cars-router.js
@@ -1,28 +1,24 @@
 // DO YOUR MAGIC
-const express = require('express')
-const Car = require('./cars-model')
-const router = express.Router()
+const express = require("express");
+const Car = require("./cars-model");
+const { checkCarId } = require("./cars-middleware");
+const router = express.Router();
 
-router.get('/', async (req, res, next) => {
-try {
-    const cars = await Car.getAll()
-    res.json(cars)
-} catch (err) {
-  next(err)
-}
-})
+router.get("/", async (req, res, next) => {
+  try {
+    const cars = await Car.getAll();
+    res.json(cars);
+  } catch (err) {
+    next(err);
+  }
+});
 
-router.get('/:id', async (req, res, next) => {
-    try {
-        const car = await Car.getById(req.params.id)
-        res.json(car)
-    } catch (err) {
-      next(err)
-    }
-})
+router.get("/:id", checkCarId, async (req, res, next) => {
+  res.json(req.car)
+});
 
-router.post('/', async (req, res, next) => {
-    res.json('posting new car')
-})
+router.post("/", async (req, res, next) => {
+  res.json("posting new car");
+});
 
-module.exports = router
+module.exports = router;
